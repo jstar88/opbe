87cd81e0558e23084f8e15139b620a83bb959b06
jstar88 <covolo.n@libero.it>
2013-04-02 14:39:11 +0200

    license

--- implementations/xgp_2_10_5.php
+++ implementations/xgp_2_10_5.php
@@ -6,25 +6,24 @@
  * This file is part of OPBE.
  * 
  * OPBE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * OPBE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with OPBE.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package OPBE
  * @author Jstar <frascafresca@gmail.com>
  * @copyright 2013 Jstar <frascafresca@gmail.com>
- * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @license http://www.gnu.org/licenses/ GNU AGPLv3 License
  * @version alpha(2013-2-4)
  * @link https://github.com/jstar88/opbe
- *
  */
 global $pricelist, $lang, $resource, $CombatCaps, $user;
 
