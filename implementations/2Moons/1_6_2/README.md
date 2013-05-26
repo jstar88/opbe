91d3a8ee490ad42dc500d37eb05b0575bd1b365a
jstar88 <covolo.n@libero.it>
2013-05-26 16:24:57 +0200

    2moons 1.6.1

---
+++ implementations/2Moons/1_6_2/README.md
@@ -0,0 +1,9 @@
+## Installation
+
+1. Download and upload to *includes/libs/opbe/* all OPBE files.
+2. Make sure that the constant *PATH* point to the correct OPBE path,defined in the implementation file as:
+    
+    ```php
+    define('PATH', ROOT_PATH . 'includes/libs/opbe/');
+    ```
+3. Replace default 2Moons's *includes/classes/missions/calculateAttack.php* with the one above 
---
+++ implementations/2Moons/1_6_2/calculateAttack.php
@@ -0,0 +1,280 @@
+<?php
+
+/**
+ *  OPBE
+ *  Copyright (C) 2013  Jstar
+ *
+ * This file is part of OPBE.
+ * 
+ * OPBE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OPBE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OPBE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OPBE
+ * @author Jstar <frascafresca@gmail.com>
+ * @copyright 2013 Jstar <frascafresca@gmail.com>
+ * @license http://www.gnu.org/licenses/ GNU AGPLv3 License
+ * @version alpha(2013-2-4)
+ * @link https://github.com/jstar88/opbe
+ */
+define('PATH', ROOT_PATH . 'includes/libs/opbe/');
+include (PATH . 'utils/includer.php');
+
+define('ID_MIN_SHIPS', 100);
+define('ID_MAX_SHIPS', 300);
+define('HOME_FLEET', 0);
+define('DEFENDERS_WON', 'r');
+define('ATTACKERS_WON', 'a');
+define('DRAW', 'w');
+define('METAL_ID', 901);
+define('CRYSTAL_ID', 902);
+
+
+/**
+ * calculateAttack()
+ * Calculate the battle using OPBE
+ * 
+ * @param array &$attackers
+ * @param array &$defenders
+ * @param mixed $FleetTF
+ * @param mixed $DefTF
+ * @return array
+ */
+function calculateAttack(&$attackers, &$defenders, $FleetTF, $DefTF)
+{
+    $CombatCaps = $GLOBALS['CombatCaps'];
+    $pricelist = $GLOBALS['pricelist'];
+
+    /********** BUILDINGS MODELS **********/
+    //attackers
+    $attackerGroupObj = new PlayerGroup();
+    foreach ($attackers as $fleetID => $attacker)
+    {
+        $player = $attacker['user'];
+        $attackerPlayerObj = $attackerGroupObj->createPlayerIfNotExist($player['id'], array(), $player['military_tech'], $player['shield_tech'], $player['defence_tech']);
+        $attackerFleetObj = new Fleet($fleetID);
+        foreach ($attacker['detail'] as $element => $amount)
+        {
+            $fighters = getFighters($element, $amount);
+            $attackerFleetObj->add($fighters);
+        }
+        $attackerPlayerObj->addFleet($attackerFleetObj);
+    }
+    //defenders
+    $defenderGroupObj = new PlayerGroup();
+    foreach ($defenders as $fleetID => $defender)
+    {
+        $player = $defender['user'];
+        $defenderPlayerObj = $defenderGroupObj->createPlayerIfNotExist($player['id'], array(), $player['military_tech'], $player['shield_tech'], $player['defence_tech']);
+        $defenderFleetObj = getFleet($fleetID);
+        foreach ($defender['def'] as $element => $amount)
+        {
+            $fighters = getFighters($element, $amount);
+            $defenderFleetObj->add($fighters);
+        }
+        $defenderPlayerObj->addFleet($defenderFleetObj);
+    }
+
+    /********** BATTLE ELABORATION **********/
+    $opbe = new Battle($attackerGroupObj, $defenderGroupObj);
+    $opbe->startBattle();
+    $report = $opbe->getReport();
+
+    /********** WHO WON **********/
+    if ($report->defenderHasWin())
+    {
+        $won = DEFENDERS_WON;
+    } elseif ($report->attackerHasWin())
+    {
+        $won = ATTACKERS_WON;
+    } else
+    {
+        $won = DRAW;
+    }
+
+    /********** ROUNDS INFOS **********/
+
+    $ROUND = array();
+    $i = 1;
+    for (; $i <= $report->getLastRoundNumber(); $i++)
+    {
+        $attackerGroupObj = $report->getPresentationAttackersFleetOnRound($i);
+        $defenderGroupObj = $report->getPresentationDefendersFleetOnRound($i);
+        $attackAmount = $attackerGroupObj->getTotalCount();
+        $defenseAmount = $defenderGroupObj->getTotalCount();
+        $attInfo = updatePlayers($attackerGroupObj, $attackers,"detail");
+        $defInfo = updatePlayers($defenderGroupObj, $defenders,"def");
+        $ROUND[$i - 1] = roundInfo($report, $attackers, $defenders, $attackerGroupObj, $defenderGroupObj, $i, $attInfo, $defInfo);
+
+
+    }
+
+    //after battle
+    $attackerGroupObj = $report->getAfterBattleAttackers();
+    $defenderGroupObj = $report->getAfterBattleDefenders();
+    $attInfo = updatePlayers($attackerGroupObj, $attackers,"detail");
+    $defInfo = updatePlayers($defenderGroupObj, $defenders,"def");
+    $ROUND[$i - 1] = roundInfo($report, $attackers, $defenders, $attackerGroupObj, $defenderGroupObj, 'END', $attInfo, $defInfo);
+
+    /********** DEBRIS **********/
+    //attackers
+    $debAtt = $report->getAttackerDebris();
+    $debAttMet = $debAtt[0];
+    $debAttCry = $debAtt[1];
+    //defenders
+    $debDef = $report->getDefenderDebris();
+    $debDefMet = $debDef[0];
+    $debDefCry = $debDef[1];
+    //total
+    $debris = array('att' => array(METAL_ID => $debAttMet, CRYSTAL_ID => $debAttCry), 'deff' => array(METAL_ID => $debDefMet, CRYSTAL_ID => $debDefCry));
+
+    /********** LOST UNITS **********/
+    $totalLost = array('att' => $report->getTotalAttackersLostUnits(), 'def' => $report->getTotalDefendersLostUnits());
+
+    /********** RETURNS **********/
+    return array(
+        'won' => $won,
+        'debris' => $debris,
+        'rw' => $ROUND,
+        'lost' => $totalLost);
+}
+
+
+/**
+ * roundInfo()
+ * Return the info required to fill $ROUND
+ * @param BattleReport $report
+ * @param array $attackers
+ * @param array $defenders
+ * @param PlayerGroup $attackerGroupObj
+ * @param PlayerGroup $defenderGroupObj
+ * @param int $i
+ * @return array
+ */
+function roundInfo(BattleReport $report, $attackers, $defenders, PlayerGroup $attackerGroupObj, PlayerGroup $defenderGroupObj, $i, $attInfo, $defInfo)
+{
+    return array(
+        'attack' => ($i == 'END') ? 0 : $report->getAttackersFirePower($i),
+        'defense' => ($i == 'END') ? 0 : $report->getDefendersFirePower($i),
+        'defShield' => ($i == 'END') ? 0 : $report->getDefendersAssorbedDamage($i),
+        'attackShield' => ($i == 'END') ? 0 : $report->getAttachersAssorbedDamage($i),
+        'attackers' => $attackers,
+        'defenders' => $defenders,
+        'attackA' => $attInfo[1],
+        'defenseA' => $defInfo[1],
+        'infoA' => $attInfo[0],
+        'infoD' => $defInfo[0]);
+}
+
+
+/**
+ * updatePlayers()
+ * Update players array as default 2moons require
+ * 
+ * @param PlayerGroup $playerGroup
+ * @param array &$players
+ * @return null
+ */
+function updatePlayers(PlayerGroup $playerGroup, &$players,$index)
+{
+    $plyArray = array();
+    $amountArray = array();
+
+    foreach ($players as $idFleet => $info)
+    {
+        $shipInfo = $info[$index];
+        $player = $playerGroup->getPlayer($info['user']['id']);
+        $fleet = ($player !== false) ? $player->getFleet($idFleet) : false;
+
+        foreach ($shipInfo as $idFighters => $amount)
+        {
+            if ($fleet !== false)
+            {
+                $fighters = $fleet->getFighters($idFighters);
+                if ($fighters !== false)
+                {
+                    $plyArray[$idFleet][$idFighters] = array(
+                        'def' => $fighters->getHull(),
+                        'shield' => $fighters->getShield(),
+                        'att' => $fighters->getPower());
+                    $players[$idFleet][$index][$idFighters] = $fighters->getCount();
+                } else
+                {
+                    $players[$idFleet][$index][$idFighters] = 0;
+                }
+            } else
+            {
+                $players[$idFleet][$index][$idFighters] = 0;
+            }
+
+            if (!isset($amountArray[$idFleet]))
+            {
+                $amountArray[$idFleet] = 0;
+            }
+            if (!isset($amountArray['total']))
+            {
+                $amountArray['total'] = 0;
+            }
+            $currentAmount = $players[$idFleet][$index][$idFighters];
+            $amountArray[$idFleet] = $amountArray[$idFleet] + $currentAmount;
+            $amountArray['total'] = $amountArray['total'] + $currentAmount;
+        }
+        $players[$idFleet]['techs'] = array(
+            ($player != false) ? $player->getWeaponsTech() : 0,
+            ($player != false) ? $player->getArmourTech() : 0,
+            ($player != false) ? $player->getShieldsTech() : 0);
+    }
+    return array($plyArray, $amountArray);
+}
+
+
+/**
+ * getFighters()
+ * Choose the correct class type by ID
+ * 
+ * @param int $id
+ * @param int $count
+ * @return a Ship or Defense instance
+ */
+function getFighters($id, $count)
+{
+    $CombatCaps = $GLOBALS['CombatCaps'];
+    $pricelist = $GLOBALS['pricelist'];
+    $rf = isset($CombatCaps[$id]['sd']) ? $CombatCaps[$id]['sd'] : 0;
+    $shield = $CombatCaps[$id]['shield'];
+    $cost = array($pricelist[$id]['metal'], $pricelist[$id]['crystal']);
+    $power = $CombatCaps[$id]['attack'];
+    if ($id > ID_MIN_SHIPS && $id < ID_MAX_SHIPS)
+    {
+        return new Ship($id, $count, $rf, $shield, $cost, $power);
+    }
+    return new Defense($id, $count, $rf, $shield, $cost, $power);
+}
+
+
+/**
+ * getFleet()
+ * Choose the correct class type by ID
+ * 
+ * @param int $id
+ * @return a Fleet or HomeFleet instance
+ */
+function getFleet($id)
+{
+    if ($id == HOME_FLEET)
+    {
+        return new HomeFleet(HOME_FLEET);
+    }
+    return new Fleet($id);
+}
+
+?>
