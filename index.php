06400b70502822a7f3d8afbb3dddf13df08105fa
jstar88 <covolo.n@libero.it>
2013-10-29 16:53:09 +0100

    info

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 
-define('WEBTEST_URL',"http://opbe.webnet32.com/tests/runnable/WebTest.php");
-
-//Detect if running locally and include files as such
-if($_SERVER['REMOTE_ADDR'] == "127.0.0.1" OR $_SERVER['REMOTE_ADDR'] == "::1") {
 	die(header("location: tests/runnable/WebTest.php"));
-}
-else
-	die(header("location: ".WEBTEST_URL));
