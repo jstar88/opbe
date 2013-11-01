f1a4904bce4184bb0abb7c7f387e0e891579461a
jstar88 <covolo.n@libero.it>
2013-11-01 19:19:04 +0100

    added a way to list errors in webtest

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-	die(header("location: tests/runnable/WebTest.php"));
+
+echo '
+<!DOCTYPE HTML>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<title>INDEX</title>
+</head>
+
+<body>
+
+<center>
+<input type="button" value="Try OPBE" onclick="window.location=\'tests/runnable/WebTest.php\'" />
+<input type="button" value="Check errors" onclick="window.location=\'tests/runnable/errors/\'" />
+</center>
+
+</body>
+</html>
+
+';
